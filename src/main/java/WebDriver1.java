33e9149b59134feebdb75d42566766d2d642b2af
Bogdan Fostenko <bfostenko@intellias.com>
2019-04-10 18:31:09 +0300

    description

--- src/main/java/WebDriver1.java
+++ src/main/java/WebDriver1.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-//launch browser
+//browser init
 public class WebDriver1 {
 
     public static void main(String[] args) {
